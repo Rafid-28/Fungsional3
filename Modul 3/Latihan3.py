ec9342922dd535a0da81136c8be55aeb9e7efd27
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2023-10-25 08:01:55 +0700

    Add files via upload

---
+++ Modul 3/Latihan3.py
@@ -0,0 +1,20 @@
+random_list = [3.1, 2.7, 5.5, 19, 753, 412, 'Hello', 'Python', 'world', 'AI']
+
+data_float = list(filter(lambda x: isinstance(x, float), random_list))
+data_int = list(filter(lambda x: isinstance(x, int), random_list))
+data_string = list(filter(lambda x: isinstance(x, str), random_list))
+
+def map_to_digits(num):
+    return {
+        'ratusan': num // 100,
+        'puluhan': (num % 100) // 10,
+        'satuan': num % 10
+    }
+
+data_int_mapped = list(map(map_to_digits, data_int))
+
+print("Data Float:", data_float)
+print("Data Int:")
+for item in data_int_mapped:
+    print(item)
+print("Data String:", data_string)
